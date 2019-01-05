3db6584537a069dae9d8a5b3f05657b9bbff54a1
capellayee <capellayee@capellas-macbook-pro.local>
2019-01-05 11:54:29 +0900

    Initial Commit. Add TableViewController DataSource and Delegate Methods.

---
+++ Wave/TodoListViewController.swift
@@ -0,0 +1,44 @@
+//
+//  TodoListViewController.swift
+//  Wave
+//
+//  Created by Capella Yee on 1/5/19.
+//  Copyright © 2019 Capella Yee. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TodoListViewController : UITableViewController {
+    
+    let itemsArray = ["meditate", "cook shroomies", "yoga"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    //MARK: TableView Datasource methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoItemCell", for: indexPath)
+        cell.textLabel?.text = itemsArray[indexPath.row]
+        return cell
+    }
+    
+    //MARK: TableView Delegate methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
